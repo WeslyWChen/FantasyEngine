2cccf14371257b553848ea39676b212852b8d43f
chenwei <306325111@qq.com>
2025-03-09 18:03:49 +0800

    init

---
+++ Example/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by WeslyChen on 2024/1/11.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "Hello, world!" << endl;
+
+    return 0;
+}
